b9aa68a0da5128e33826d262120911b695f30972
Paul Gerbaux <paul.gerbaux@gmail.com>
2023-08-01 22:10:18 +0200

    maj 2.2

--- FrontEnd/api-login.js
+++ FrontEnd/api-login.js
@@ -9,18 +9,36 @@ document.addEventListener("DOMContentLoaded", function () {
     const email = emailInput.value;
     const password = passwordInput.value;
 
-    // Vérification des informations d'authentification
-    if (email === "sophie.bluel@test.tld" && password === "S0phie") {
-      // Informations correctes : stockage du token d'authentification dans le localstorage
-      localStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4");
+    // Création de l'objet avec les informations d'authentification
+    const userCredentials = {
+      email: email,
+      password: password
+    };
 
-      // Redirection vers la page d'accueil (ou autre page protégée)
-      window.location.href = "FrontEnd/admin.html";
-    } else {
-      // Informations incorrectes : affichage d'un message d'erreur
-      alert("Informations d'identification incorrectes. Veuillez réessayer.");
-    }
+    // Envoi des informations d'authentification via une requête POST
+    fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userCredentials)
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Vérification de la réponse de l'API
+        if (data.userId && data.token) {
+          // Informations correctes : stockage du token d'authentification dans le localstorage
+          localStorage.setItem("token", data.token);
+
+          // Redirection vers la page d'accueil
+          window.location.href = "index.html";
+        } else {
+          // Informations incorrectes : affichage d'un message d'erreur
+          alert("Informations d'identification incorrectes. Veuillez réessayer.");
+        }
+      })
+      .catch(error => {
+        console.error("Une erreur s'est produite : ", error);
+      });
   });
 });
-
-
--- FrontEnd/api.js
+++ FrontEnd/api.js
@@ -88,3 +88,58 @@ fetch('http://localhost:5678/api/works')
 }
 
 
+//Etape 2.2
+
+// Vérification de la présence du token dans le localstorage
+const token = localStorage.getItem('token');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const loginButton = document.querySelector('nav ul li a');
+  
+  if (token) {
+    // Si le token est présent, changer le texte du bouton en "Logout"
+    loginButton.textContent = 'logout';
+    // Ajouter un événement au bouton "Logout" pour gérer la déconnexion
+    loginButton.addEventListener('click', handleLogout);
+
+    // Créer et ajouter l'élément <div> avec la classe "login" au <header> (uniquement si le token est présent)
+    const header = document.querySelector('body');
+    const loginDiv = document.createElement('div');
+    loginDiv.className = 'login';
+
+    // Créer l'élément <i> pour le mode édition
+    const editIcon = document.createElement('i');
+    editIcon.className = 'fa-regular fa-pen-to-square';
+    const editModeText = document.createTextNode(' Mode édition ');
+    const editModeContainer = document.createElement('div');
+    editModeContainer.appendChild(editIcon);
+    editModeContainer.appendChild(editModeText);
+    editModeContainer.className = 'edit-mode';
+
+    // Créer l'élément <div> pour la publication des changements avec la classe "publish"
+    const publishContainer = document.createElement('div');
+    publishContainer.textContent = 'publier les changements';
+    publishContainer.className = 'publish';
+
+    // Ajouter les éléments <div> dans le <header> (sous la classe "login")
+    loginDiv.appendChild(editModeContainer);
+    loginDiv.appendChild(publishContainer);
+    header.insertBefore(loginDiv, header.firstChild);
+  } else {
+    // Si le token n'est pas présent, le bouton reste "Login"
+    loginButton.textContent = 'login';
+    // Ajouter un événement au bouton "Login" pour gérer la connexion
+    loginButton.addEventListener('click', handleLogin);
+  }
+});
+
+// Fonction pour gérer la déconnexion (supprimer le token et rediriger vers login.html)
+function handleLogout() {
+  localStorage.removeItem('token');
+  window.location.href = 'login.html';
+}
+
+// Fonction pour gérer la connexion (simplement rediriger vers login.html)
+function handleLogin() {
+  window.location.href = 'login.html';
+}
